cf925ce411d392f99bb7df5473b9f6118be082ae
joao rabelo <jh.rabelo7@gmail.com>
2026-04-22 01:18:27 -0300

    conexao com  o banco de dados

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$db   = "educacaofinaceira";
+$user = "root";
+$pass = "";   
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conectado com sucesso!";
+} catch (PDOException $e) {
+    echo "Erro na conexão: " . $e->getMessage();
+}
+?>
