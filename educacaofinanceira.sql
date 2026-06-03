3ced8b1ee8ddf80266ba02a4a59c0dd2740f1eae
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2026-06-03 14:09:55 -0300

    Add files via upload

--- educacaofinanceira.sql
+++ educacaofinanceira.sql
@@ -3,9 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29-Abr-2026 às 19:02
+-- Tempo de geração: 20-Maio-2026 às 19:22
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.0.25
+
 CREATE DATABASE IF NOT EXISTS educacaofinanceira;
 USE educacaofinanceira;
 
@@ -29,7 +30,7 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
+CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -42,63 +43,12 @@ CREATE TABLE `usuarios` (
 
 --
 -- Extraindo dados da tabela `usuarios`
-USE educacaofinanceira;
-
-DROP TABLE IF EXISTS usuarios;
-
+--
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `telefone`, `data_criacao`) VALUES
 (1, 'ana portela', 'anaportela@gmail.com', '$2y$10$AQak0pbULnxpniQnTOmFcuZtVDIfBPDxH4SqGxscr9ot.2ExymF9i', '', '2026-04-29 16:09:29'),
 (2, 'isadora', 'isadorajans@gmail.com', '$2y$10$SMNthbFKZrKhI0BD5pOUz.bzQOHON1NO5i4BErJas7EMvded0jzKS', '11875938264', '2026-04-29 16:30:02'),
 (3, 'JoÃ£o Rabelo', 'rabelo@gmail.com', '$2y$10$P8D0Jm6eXaT0uQuTTnuQru4dZtnm2IcTO.IW1tf8kDHjX7dh8gvsu', '', '2026-04-29 16:46:02');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 20-Maio-2026 às 19:22
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `educacaofinanceira`
---
 
 -- --------------------------------------------------------
 
@@ -106,35 +56,17 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `movimentacoes`
 --
 
-CREATE TABLE `movimentacoes` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `movimentacoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `usuario_id` int(11) NOT NULL,
   `tipo` enum('entrada','saida') NOT NULL,
   `valor` decimal(10,2) NOT NULL,
   `descricao` varchar(255) DEFAULT NULL,
   `categoria` varchar(100) DEFAULT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `movimentacoes`
---
-ALTER TABLE `movimentacoes`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `movimentacoes`
---
-ALTER TABLE `movimentacoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
