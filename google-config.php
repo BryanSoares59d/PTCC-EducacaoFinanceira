94cfe4775d7abca2daa0b69fa49a1f2694d79fe1
Joaorabelo7 <jh.rabelo7@gmail.com>
2026-07-05 10:15:37 -0300

    Remove google-config.php do controle de versão

--- google-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-
-define('GOOGLE_CLIENT_ID', '***REMOVED***');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URL', 'http://localhost/ptcc-educacaofinanceira/google-callback.php');
-
-
-?>
